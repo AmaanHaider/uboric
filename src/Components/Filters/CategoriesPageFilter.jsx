0535e443ef7ca09d6101fc716a705fb8112dee16
WFH21 <amaanhaider270@gmail.com>
2022-05-06 15:12:43 +0530

    UPDATE

--- src/Components/Filters/CategoriesPageFilter.jsx
+++ src/Components/Filters/CategoriesPageFilter.jsx
@@ -1,9 +1,107 @@
 import React from 'react'
 import "./Styles/CategoriesPageFilter.css"
 
+
+
 const CategoriesPageFilter = () => {
   return (
-    <div>CategoriesPageFilter</div>
+    <div className='categoryfiltercontainer'>
+      <div className='categoryfilter'>
+        <h5>Product Categories</h5>
+        <p><input type="checkbox" name="" id="" />Trimmer</p>
+        <p><input type="checkbox" name="" id="" />Men's Footwear</p>
+        <p><input type="checkbox" name="" id="" />Casual Chapple/Slippers</p>
+        <p><input type="checkbox" name="" id="" />Casual Sandals</p>
+        <p><input type="checkbox" name="" id="" />Casual/Party Shoes</p>
+        <p><input type="checkbox" name="" id="" />Chappal/Slippers</p>
+        <p><input type="checkbox" name="" id="" />Flip FLops</p>
+        <p><input type="checkbox" name="" id="" />Loafers</p>
+        <p><input type="checkbox" name="" id="" />Mojdi</p>
+        <p><input type="checkbox" name="" id="" />Sneakers</p>
+        <p><input type="checkbox" name="" id="" />Socks</p>
+        <p><input type="checkbox" name="" id="" />Sports sandal</p>
+
+        <p><input type="checkbox" name="" id="" />50% of</p>
+
+        <p><input type="checkbox" name="" id="" />Socks</p>
+
+      </div>
+
+      <div className='categoryfilter'>
+        <h5>By Size</h5>
+        <p><input type="checkbox" name="" id="" />S (1)</p>
+        <p><input type="checkbox" name="" id="" />XL (2)</p>
+        <p><input type="checkbox" name="" id="" />L (2)</p>
+        <p><input type="checkbox" name="" id="" />M (2)</p>
+        <p><input type="checkbox" name="" id="" />XXL (2)</p>
+        <p><input type="checkbox" name="" id="" />1 (3)</p>
+        <p><input type="checkbox" name="" id="" />1.5 (2)</p>
+        <p><input type="checkbox" name="" id="" />2 (2)</p>
+        <p><input type="checkbox" name="" id="" />2.5 (2)</p>
+        <p><input type="checkbox" name="" id="" />3 (4)</p>
+        <p><input type="checkbox" name="" id="" />4 (2)</p>
+        <p><input type="checkbox" name="" id="" />5 (2)</p>
+        <p><input type="checkbox" name="" id="" />5 (2)</p>
+        <p><input type="checkbox" name="" id="" />5 (4)</p>
+        <p><input type="checkbox" name="" id="" />5 (3)</p>
+        <p><input type="checkbox" name="" id="" />5.5 (1)</p>
+        <p><input type="checkbox" name="" id="" />6 (50)</p>
+        <p><input type="checkbox" name="" id="" />6.5 (3)</p>
+        <p><input type="checkbox" name="" id="" />7 (2)</p>
+        <p><input type="checkbox" name="" id="" />7.5 (2)</p>
+        <p><input type="checkbox" name="" id="" />8 (166)</p>
+          <p><input type="checkbox" name="" id="" />8.5 (172)</p>
+          <p><input type="checkbox" name="" id="" />9 (142)</p>
+          <p><input type="checkbox" name="" id="" />9.5 (2)</p>
+          <p><input type="checkbox" name="" id="" />10 (160)</p>
+          <p><input type="checkbox" name="" id="" />11 (42)</p>
+          <p><input type="checkbox" name="" id="" />11.12 (2)</p>
+          <p><input type="checkbox" name="" id="" />12 (13)</p>
+          <p><input type="checkbox" name="" id="" />13 (2)</p>
+          <p><input type="checkbox" name="" id="" />13.5 (2)</p>
+          <p><input type="checkbox" name="" id="" />14 (2)</p>
+          <p><input type="checkbox" name="" id="" />36 (2)</p>
+          <p><input type="checkbox" name="" id="" />37 (3)</p>
+          <p><input type="checkbox" name="" id="" />38 (2)</p>
+          <p><input type="checkbox" name="" id="" />40 (9)</p>
+          <p><input type="checkbox" name="" id="" />41 (14)</p>
+          <p><input type="checkbox" name="" id="" />42 (13)</p>
+          <p><input type="checkbox" name="" id="" />43 (13)</p>
+          <p><input type="checkbox" name="" id="" />44 (7)</p>
+          <p><input type="checkbox" name="" id="" />45 (7)</p>
+          <p><input type="checkbox" name="" id="" />91 (2)</p>
+          <p><input type="checkbox" name="" id="" />111 (2)</p>
+
+    </div>
+
+    <div className='categoryfilter'>
+        <h5>Product Categories</h5>
+        <p><input type="checkbox" name="" id="" />BEIGE (1)</p>
+        <p><input type="checkbox" name="" id="" />BLACK (2)</p>
+        <p><input type="checkbox" name="" id="" />BALCK/BLUE (1)</p>
+        <p><input type="checkbox" name="" id="" />BLACK/WHITE (2)</p>
+        <p><input type="checkbox" name="" id="" />BLUE/GREY (1)</p>
+        <p><input type="checkbox" name="" id="" />BROWN (5)</p>
+        <p><input type="checkbox" name="" id="" />CAMEL (3)</p>
+        <p><input type="checkbox" name="" id="" />CHERY (1)</p>
+        <p><input type="checkbox" name="" id="" />E.TAN (1)</p>
+        <p><input type="checkbox" name="" id="" />GREY/BLUE (1) </p>
+        <p><input type="checkbox" name="" id="" />NAVY BLUE (1)</p>
+        <p><input type="checkbox" name="" id="" />NAVY/RED (5)</p>
+        <p><input type="checkbox" name="" id="" />RUST (2)</p>
+        <p><input type="checkbox" name="" id="" />TAN(5)</p>
+        <p><input type="checkbox" name="" id="" />TOBACCO (1)</p>
+        <p><input type="checkbox" name="" id="" />WHITE/BALCK (1)</p>
+    </div>
+    <div className='categoryfilter'>  
+    <p><input type="checkbox" name="" id="" />LACE UP (31)</p>
+    <p><input type="checkbox" name="" id="" />SLIP ON (67)</p>
+    
+    </div>
+
+    <button className="catfilterbutton"  >CLEAR</button>
+
+    </div>
   )
 }
 
